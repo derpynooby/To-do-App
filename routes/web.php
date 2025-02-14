daf9793a3d006dccd86847dfbb45c73e9d68635e
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-14 11:18:59 +0700

    Registering the controller to route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// dahboard route
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+// project route
+route::resource('projects', ProjectController::class);
+
+// task route
+route::resource('tasks', TaskController::class);
