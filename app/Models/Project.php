a82628ab04bbc193d5827db765ecdcf2c0ed8a8e
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-14 10:57:49 +0700

    Editing project and task model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
-    //
+    // Adding factory for testing
+    use HasFactory;
+
+    /**
+     * Adding mass assignment
+     * Set of fillable fields
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'deadline',
+        'status',
+        'priority',
+    ];
+
+    /**
+     * Adding guard
+     * Set of guarded fields
+     * @var array
+     */
+    protected $guarded = [$id];
+
+
+    /**
+     * Get all of the tasks for the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Adding mass assignment.
+     * Set of fillable fields.
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'priority',
+        'project_id',
+    ];
+
+    /**
+     * Adding guard
+     * Set of guarded fields
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Get the project that owns the task.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function project():BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
