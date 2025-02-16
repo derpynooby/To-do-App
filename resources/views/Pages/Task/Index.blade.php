c28e5ec853bd5ed2d3a25b02ccc44f04e659a4c7
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-16 19:48:37 +0700

    Edit index for testing

--- resources/views/Pages/Task/Index.blade.php
+++ resources/views/Pages/Task/Index.blade.php
@@ -7,96 +7,19 @@
 @section('content')
     <h1>Tasks</h1>
     <div class="d-flex justify-content-between align-items-center">
+        @forelse ($tasks as $task)
         <div class="grid grid-cols-3 gap-4">
-            <h2 class="">Store</h2>
-            <form action="{{ route('tasks.store') }}" class="d-flex flex-column gap 3" method="post">
-                @method("post")
-                @csrf
-                <div class="mb-3">
-                    <label for="name">Task Name</label>
-                    <input type="text" name="name" id="name">
-                </div>
-                <div class="mb-3">
-                    <label for="description">Description</label>
-                    <input type="textarea" name="description" id="description">
-                </div>
-                <div class="mb-3">
-                    <label for="status">Status</label>
-                    <select name="status" id="status">
-                        <option value="pending">pending</option>
-                        <option value="in_progress">in progress</option>
-                        <option value="completed">completed</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="priority">Priority</label>
-                    <select name="priority" id="priority">
-                        <option value="low">low</option>
-                        <option value="medium">medium</option>
-                        <option value="high">high</option>
-                    </select>                
-                </div>
-                <div class="mb-3">
-                    <label for="project_id">Project Id</label>
-                    <input type="number" name="project_id" id="project_id">
-                </div>
-                <div class="mb-3">
-                    <button class="button button-primary">Submit</button>
-                </div>
-            </form>
-        </div>
-        <div class="grid grid-cols-3 gap-4">
-            <h2 class="">Update</h2>
-            <form action="{{ route('tasks.update', $task->id) }}" class="d-flex flex-column gap 3" method="post">
-                @method("patch")
-                @csrf
-                <div class="mb-3">
-                    <label for="name">task Name</label>
-                    <input type="text" name="name" id="name">
-                </div>
-                <div class="mb-3">
-                    <label for="description">Description</label>
-                    <input type="textarea" name="description" id="description">
-                </div>
-                <div class="mb-3">
-                    <label for="deadline">Deadline</label>
-                    <input type="date" name="deadline" id="deadline">
-                </div>
-                <div class="mb-3">
-                    <label for="status">Status</label>
-                    <select name="status" id="status">
-                        <option value="pending">pending</option>
-                        <option value="in_progress">in progress</option>
-                        <option value="completed">completed</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="priority">Priority</label>
-                    <select name="priority" id="priority">
-                        <option value="low">low</option>
-                        <option value="medium">medium</option>
-                        <option value="high">high</option>
-                    </select>                
-                </div>
-                <div class="mb-3">
-                    <button class="button button-primary">Submit</button>
-                </div>
-            </form>
-        </div>
-            
-        <div class="grid grid-cols-3 gap-4">
-            @forelse ($tasks as $task)
                 <div class="bg-white rounded shadow">
                     <a href="/tasks/{{ $task->id }}">
                         <img src="/img/1.jpg" alt="task Image" class="rounded-t">
                     </a>
                     <div class="p-4">
-                        <h2 class="text-lg font-bold mb-2"><a href="/tasks/{{ $task->id }}">{{ $task->title }}</a></h2>
+                        <h2 class="text-lg font-bold mb-2"><a href="/tasks/{{ $task->id }}">{{ $task->name }}</a></h2>
                         <p class="text-gray-600">{{ $task->description }}</p>
                     </div>
                     <div class="p-4">
                         <h2 class="">Delete</h2>
-                        <form action="{{ route('tasks.delete',  $task->id) }}" class="d-flex flex-column gap 3" method="post">
+                        <form action="{{ route('tasks.destroy',  $task->id) }}" class="d-flex flex-column gap 3" method="post">
                             @method("delete")
                             @csrf
                             <div class="mb-3">
@@ -105,14 +28,91 @@
                         </form>
                     </div>
                 </div>
+                <div class="grid grid-cols-3 gap-4">
+                    <h2 class="">Update</h2>
+                    <form action="{{ route('tasks.update', $task->id) }}" class="d-flex flex-column gap 3" method="post">
+                        @method("patch")
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name">task Name</label>
+                            <input type="text" name="name" id="name" value="{{ $task->name }}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="description">Description</label>
+                            <input type="textarea" name="description" id="description" value="{{ $task->description }}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="status">Status</label>
+                            <select name="status" id="status">
+                                <option value="pending" @selected($task->status == 'pending')>pending</option>
+                                <option value="in_progress" @selected($task->status == 'in_progress')>in progress</option>
+                                <option value="completed" @selected($task->status == 'completed')>completed</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="priority">Priority</label>
+                            <select name="priority" id="priority">
+                                <option value="low" @selected($task->priority == 'low')>low</option>
+                                <option value="medium" @selected($task->priority == 'medium')>medium</option>
+                                <option value="high" @selected($task->priority == 'high')>high</option>
+                            </select>                
+                        </div>
+                        <div class="mb-3">
+                            <label for="project_id">Project Id</label>
+                            <input type="number" name="project_id" id="project_id" value="{{ $task->project_id }}">
+                        </div>
+                        <div class="mb-3">
+                            <button class="button button-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                    
+                
+            </div>
+
             @empty
-                <p>No tasks found.</p>
+            <div class="grid grid-cols-3 gap-4">
+                <h2 class="">Store</h2>
+                <form action="{{ route('tasks.store') }}" class="d-flex flex-column gap 3" method="post">
+                    @method("post")
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name">Task Name</label>
+                        <input type="text" name="name" id="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description">Description</label>
+                        <input type="textarea" name="description" id="description">
+                    </div>
+                    <div class="mb-3">
+                        <label for="status">Status</label>
+                        <select name="status" id="status">
+                            <option value="pending">pending</option>
+                            <option value="in_progress">in progress</option>
+                            <option value="completed">completed</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="priority">Priority</label>
+                        <select name="priority" id="priority">
+                            <option value="low">low</option>
+                            <option value="medium">medium</option>
+                            <option value="high">high</option>
+                        </select>                
+                    </div>
+                    <div class="mb-3">
+                        <label for="project_id">Project Id</label>
+                        <input type="number" name="project_id" id="project_id">
+                    </div>
+                    <div class="mb-3">
+                        <button class="button button-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
             @endforelse
         </div>
-    </div>
-    <div class="mt-4">
-        {{ $tasks->links() }}
-    </div>
+        
+       
 @endSection 
 
 @section('footer')
