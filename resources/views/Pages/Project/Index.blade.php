4b37e459946b5a1c51978a7482f764903a071038
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-18 12:14:37 +0700

    adding views for the pages

--- resources/views/Pages/Project/Index.blade.php
+++ resources/views/Pages/Project/Index.blade.php
@@ -1,121 +1,103 @@
-@extends('Layouts.app')
+@extends('layouts.app')
 
-@section('header')
-    {{-- @include('layouts.header') --}}
-@endSection
+
 
 @section('content')
-    <h1>Projects</h1>
-    <div class="grid grid-cols-3 gap-4">
-        @forelse ($projects as $project)
-            <div class="bg-white rounded shadow">
-                <a href="/projects/{{ $project->id }}">
-                    <img src="/img/1.jpg" alt="Project Image" class="rounded-t">
-                </a>
-                <div class="p-4">
-                    <h2 class="text-lg font-bold mb-2"><a href="/projects/{{ $project->id }}">{{ $project->name }}</a></h2>
-                    <p class="text-gray-600">{{ $project->description }}</p>
-                </div>
-                <div class="p-4">
-                    <h2 class="">Delete</h2>
-                    <form action="{{ route('projects.destroy',  $project->id) }}" class="d-flex flex-column gap 3" method="post">
-                        @method("delete")
-                        @csrf
-                        <div class="mb-3">
-                            <button class="button button-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-          
-   
-    
-        <div class="grid grid-cols-3 gap-4">
-            <h2 class="">Update</h2>
-            <form action="{{ route('projects.update', $project->id) }}" class="d-flex flex-column gap 3" method="post">
-                @method("patch")
-                @csrf
-                <div class="mb-3">
-                    <label for="name">Project Name</label>
-                    <input type="text" name="name" id="name">
-                </div>
-                <div class="mb-3">
-                    <label for="description">Description</label>
-                    <input type="textarea" name="description" id="description">
-                </div>
-                <div class="mb-3">
-                    <label for="deadline">Deadline</label>
-                    <input type="date" name="deadline" id="deadline">
-                </div>
-                <div class="mb-3">
-                    <label for="status">Status</label>
-                    <select name="status" id="status">
-                        <option value="pending">pending</option>
-                        <option value="in_progress">in progress</option>
-                        <option value="completed">completed</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="priority">Priority</label>
-                    <select name="priority" id="priority">
-                        <option value="low">low</option>
-                        <option value="medium">medium</option>
-                        <option value="high">high</option>
-                    </select>                
-                </div>
-                <div class="mb-3">
-                    <button class="button button-primary">Submit</button>
-                </div>
-            </form>
-        </div>
-            
-       
-    </div>
-    @empty
 
-    <div class="d-flex justify-content-between align-items-center">
-        <div class="grid grid-cols-3 gap-4">
-            <h2 class="">Store</h2>
-            <form action="{{ route('projects.store') }}" class="d-flex flex-column gap 3" method="post">
-                @method("post")
-                @csrf
-                <div class="mb-3">
-                    <label for="name">Project Name</label>
-                    <input type="text" name="name" id="name">
+        <div class="d-flex justify-content-center align-items-center">
+            <x-card style="width: 100%; height:75vh;">
+                <div class="d-flex justify-content-between align-items-center border-bottom border-dark pb-3 px-3">
+                    <text class="fs-5">Projects List</text>
+                    <x-modal id="ModalAdd">
+                        <x-slot:buttonOpen>
+                            <x-button type="modal" data-bs-target="#ModalAdd">Add Project</x-button>
+                        </x-slot>
+                        <form id="addProjectForm" action="{{ route('projects.store') }}" class="d-flex flex-column gap 3" method="post">
+                            @method("post")
+                            @csrf
+                            <div class="mb-4">
+                                <x-input type="text" name="name" label="Project Name" placeholder="Enter your project name"></x-input>
+                                <x-input type="textarea" name="description" label="Description" placeholder="Enter your project description" helpText="you can add explanation about your project"></x-input>
+                                <x-input type="date" name="deadline" label="Deadline (optional)" placeholder="Choose your project deadline"></x-input>
+                            </div>
+                            <div class="mb-1">
+                                <x-input type="select" name="status" label="Status" placeholder="Choose your project status">
+                                    <option value="pending">pending</option>
+                                    <option value="in_progress">in progress</option>
+                                    <option value="completed">completed</option>
+                                </x-input>
+                                <x-input type="select" name="priority" label="Priority" placeholder="Choose your project priority">
+                                    <option value="low">low</option>
+                                    <option value="medium">medium</option>
+                                    <option value="high">high</option>
+                                </x-input>
+                            </div>
+                            <x-slot:buttonClose>
+                                <x-button type="cancel">Cancel</x-button>
+                            </x-slot>
+                            <x-slot:buttonSave>
+                                <x-button type="submit" formId="addProjectForm">Submit</x-button>
+                            </x-slot>
+                        </form>
+                    </x-modal>
                 </div>
-                <div class="mb-3">
-                    <label for="description">Description</label>
-                    <input type="textarea" name="description" id="description">
-                </div>
-                <div class="mb-3">
-                    <label for="deadline">Deadline</label>
-                    <input type="date" name="deadline" id="deadline">
-                </div>
-                <div class="mb-3">
-                    <label for="status">Status</label>
-                    <select name="status" id="status">
-                        <option value="pending">pending</option>
-                        <option value="in_progress">in progress</option>
-                        <option value="completed">completed</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="priority">Priority</label>
-                    <select name="priority" id="priority">
-                        <option value="low">low</option>
-                        <option value="medium">medium</option>
-                        <option value="high">high</option>
-                    </select>                
-                </div>
-                <div class="mb-3">
-                    <button class="button button-primary">Submit</button>
-                </div>
-            </form>
+                {{-- send $projects to accordion for loop --}}
+                <div class="accordion accordion-flush" id="accordionExample">
+                    @forelse ($projects as $project)
+                    <div class="accordion-item">
+                      <text class="accordion-header d-flex align-items-center">
+                        <button class="accordion-button collapsed" style="background-color: #ffffff;" type="button" data-bs-toggle="collapse" data-bs-target="#{{ $project->id }}" aria-expanded="true" aria-controls="collapse">
+                            {{ $project->name }}        
+                        </button>
+                        <x-modal id="ModalEdit{{ $project->id }}">            
+                            <x-slot:buttonOpen>
+                                <x-button type="edit" data-bs-target="#ModalEdit{{ $project->id }}"></x-button>
+                            </x-slot>
+                            <form id="editProjectForm{{ $project->id }}" action="{{ route('projects.update', $project->id) }}" class="d-flex flex-column gap 3" method="post">
+                                @method("patch")
+                                @csrf
+                                <div class="mb-4">
+                                    <x-input type="text" name="name" label="Project Name" placeholder="Enter your project name" value="{{ old('name', $project->name) }}"></x-input>
+                                    <x-input type="textarea" name="description" label="Description" placeholder="Enter your project description" value="{{ old('description',$project->description) }}" helpText="you can add explanation about your project"></x-input>
+                                    <x-input type="date" name="deadline" label="Deadline (optional)" placeholder="Choose your project deadline" value="{{ old('deadline', $project->deadline) }}"></x-input>
+                                </div>
+                                <div class="mb-1">
+                                    <x-input type="select" name="status" label="Status" placeholder="Choose your project status">
+                                        @foreach(['pending', 'in_progress', 'completed'] as $status)
+                                            <option value="{{ $status }}" {{ $project->status == $status ? 'selected' : '' }}>{{ ucfirst($status) }}</option>
+                                        @endforeach                    </x-input>
+                                    <x-input type="select" name="priority" label="Priority" placeholder="Choose your project priority">
+                                        @foreach(['low', 'medium', 'high'] as $priority)
+                                            <option value="{{ $priority }}" {{ $project->priority == $priority ? 'selected' : '' }}>{{ ucfirst($priority) }}</option>
+                                        @endforeach                    </x-input>
+                                </div>
+                                <x-slot:buttonClose>
+                                    <x-button type="cancel">Cancel</x-button>
+                                </x-slot>
+                                <x-slot:buttonSave>
+                                    <x-button type="submit" formId="editProjectForm{{ $project->id }}">Submit</x-button>
+                                </x-slot>
+                            </form>
+                        </x-modal>
+                        <x-button type="delete" url="{{ route('projects.destroy', $project->id) }}"></x-button>
+                    </text>
+                      <div id="{{ $project->id }}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            {{ $project->description }}
+                            <div class="">
+                                <strong> {{ $project->status }} </strong> {{ $project->priority }}
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    @empty
+                    <div class="d-flex justify-content-center align-items-center text-center" style="height: 60vh;">
+                        <h4 class="font-monospace"><strong>No project found,</strong> check out project history or create a <a href="#" data-bs-toggle="modal" data-bs-target="#ModalAdd">new one</a></h4>
+                    </div>
+                    @endforelse
+                  </div>            </x-card>
         </div>
-    @endforelse
-</div>
+        
+       
 @endSection 
 
-@section('footer')
-    {{-- @include('layouts.footer') --}}
-@endSection
--- resources/views/Pages/Task/Index.blade.php
+++ resources/views/Pages/Task/Index.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 
-@section('header')
-    {{-- @include('layouts.header') --}}
-@endSection
+
 
 @section('content')
-    <h1>Tasks</h1>
-    <div class="d-flex justify-content-between align-items-center">
+
+    {{-- <h1>Tasks</h1>
+    <div class=" d-flex flex-column-reverse justify-content-end flex-lg-row" style="height: 85vh">
         @forelse ($tasks as $task)
         <div class="grid grid-cols-3 gap-4">
                 <div class="bg-white rounded shadow">
@@ -111,10 +110,123 @@
             </div>
             @endforelse
         </div>
+        <x-modal id="Modal">
+            <x-slot:button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal">
+                    Launch demo modal
+                  </button>
+                  <x-button type="modal" data-bs-toggle="modal" data-bs-target="#Modal">test</x-button>
+            </x-slot>
+            jannnnn
+        </x-modal >
+        <x-modal id="Modal2">
+            <x-slot:button>
+                <button type="button" class="btn btn-primary" data-bs-target="#Modal">
+                    Launch demo modal
+                  </button>
+                  <x-button type="modal" data-bs-target="#Modal2">test</x-button>
+            </x-slot>
+            hahahahahahahah
+        </x-modal> --}}
+
+        <div class="d-flex justify-content-center align-items-center">
+            <x-card style="width: 100%; height:75vh;">
+                <div class="d-flex justify-content-between align-items-center border-bottom border-dark pb-3 px-3">
+                    <text class="fs-5">Tasks List</text>
+                    <x-modal id="ModalAdd">
+                        <x-slot:buttonOpen>
+                            <x-button type="modal" data-bs-target="#ModalAdd">Add Task</x-button>
+                        </x-slot>
+                        <form id="addTaskForm" action="{{ route('tasks.store') }}" class="d-flex flex-column gap 3" method="post">
+                            @method("post")
+                            @csrf
+                            <div class="mb-4">
+                                <x-input type="text" name="name" label="Task Name" placeholder="Enter your task name"></x-input>
+                                <x-input type="textarea" name="description" label="Description" placeholder="Enter your task description" helpText="you can add explanation about your task"></x-input>
+                            </div>
+                            <div class="mb-1">
+                                <x-input type="select" name="status" label="Status" placeholder="Choose your task status">
+                                    <option value="pending">pending</option>
+                                    <option value="in_progress">in progress</option>
+                                    <option value="completed">completed</option>
+                                </x-input>
+                                <x-input type="select" name="priority" label="Priority" placeholder="Choose your task priority">
+                                    <option value="low">low</option>
+                                    <option value="medium">medium</option>
+                                    <option value="high">high</option>
+                                </x-input>
+                                {{-- <x-input type="hidden" name="project_id"></x-input> --}}
+                                <div class="mb-3">
+                                    <label for="project_id">Project Id</label>
+                                    <input type="number" name="project_id" id="project_id">
+                                </div>
+                            </div>
+                            <x-slot:buttonClose>
+                                <x-button type="cancel">Cancel</x-button>
+                            </x-slot>
+                            <x-slot:buttonSave>
+                                <x-button type="submit" formId="addTaskForm">Submit</x-button>
+                            </x-slot>
+                        </form>
+                    </x-modal>
+                </div>
+                {{-- send $tasks to accordion for loop --}}
+                <div class="accordion accordion-flush" id="accordionExample">
+                    @forelse ($tasks as $task)
+                    <div class="accordion-item">
+                      <text class="accordion-header d-flex align-items-center">
+                        <button class="accordion-button collapsed" style="background-color: #ffffff;" type="button" data-bs-toggle="collapse" data-bs-target="#{{ $task->id }}" aria-expanded="true" aria-controls="collapse">
+                            {{ $task->name }}        
+                        </button>
+                        <x-modal id="ModalEdit{{ $task->id }}">            
+                            <x-slot:buttonOpen>
+                                <x-button type="edit" data-bs-target="#ModalEdit{{ $task->id }}"></x-button>
+                            </x-slot>
+                            <form id="editTaskForm{{ $task->id }}" action="{{ route('tasks.update', $task->id) }}" class="d-flex flex-column gap 3" method="post">
+                                @method("patch")
+                                @csrf
+                                <div class="mb-4">
+                                    <x-input type="text" name="name" label="Task Name" placeholder="Enter your task name" value="{{ old('name', $task->name) }}"></x-input>
+                                    <x-input type="textarea" name="description" label="Description" placeholder="Enter your task description" value="{{ old('description',$task->description) }}" helpText="you can add explanation about your task"></x-input>
+                                </div>
+                                <div class="mb-1">
+                                    <x-input type="select" name="status" label="Status" placeholder="Choose your task status">
+                                        @foreach(['pending', 'in_progress', 'completed'] as $status)
+                                            <option value="{{ $status }}" {{ $task->status == $status ? 'selected' : '' }}>{{ ucfirst($status) }}</option>
+                                        @endforeach                    </x-input>
+                                    <x-input type="select" name="priority" label="Priority" placeholder="Choose your task priority">
+                                        @foreach(['low', 'medium', 'high'] as $priority)
+                                            <option value="{{ $priority }}" {{ $task->priority == $priority ? 'selected' : '' }}>{{ ucfirst($priority) }}</option>
+                                        @endforeach                    </x-input>
+                                    <x-input type="hidden" name="project_id" value="{{ $task->project_id }}"></x-input>
+                                </div>
+                                <x-slot:buttonClose>
+                                    <x-button type="cancel">Cancel</x-button>
+                                </x-slot>
+                                <x-slot:buttonSave>
+                                    <x-button type="submit" formId="editTaskForm{{ $task->id }}">Submit</x-button>
+                                </x-slot>
+                            </form>
+                        </x-modal>
+                        <x-button type="delete" url="{{ route('tasks.destroy', $task->id) }}"></x-button>
+                    </text>
+                      <div id="{{ $task->id }}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            {{ $task->description }}
+                            <div class="">
+                                <strong> {{ $task->status }} </strong> {{ $task->priority }}
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    @empty
+                    <div class="d-flex justify-content-center align-items-center text-center" style="height: 60vh;">
+                        <h4 class="font-monospace"><strong>No task found,</strong> check out task history or create a <a href="#" data-bs-toggle="modal" data-bs-target="#ModalAdd">new one</a></h4>
+                    </div>
+                    @endforelse
+                  </div>            </x-card>
+        </div>
         
        
 @endSection 
 
-@section('footer')
-    {{-- @include('layouts.footer') --}}
-@endSection
