0eb296c685b374672d237094dbbb5518fe464564
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-18 12:40:25 +0700

    Editing index method for sending a data to view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Task;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+        $tasks = Task::all();
+        return view('dashboard', compact('projects', 'tasks'));
+    }
 }
